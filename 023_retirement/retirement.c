aa556184abb70058b2ed0615908bdafa6d60cd17
ldw59 <ldw59@duke.edu>
2025-09-07 17:22:33 -0400

    Did assignment 23

---
+++ 023_retirement/retirement.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _retire_info {
+  int months;
+  double contribution;
+  double rate_of_return;
+} retire_info;
+
+double compound_earned(double startAmount, double contributeAmount, double rate) {
+  return startAmount * (1 + rate / 12) + contributeAmount;
+}
+
+void retirement(int startAge, double initial, retire_info working, retire_info retired) {
+  int working_months = 0;
+  int retire_months = 0;
+  while (working_months < working.months) {
+    int year = startAge / 12;
+    int month = startAge % 12;
+    printf("Age %3d month %2d you have $%.2f\n", year, month, initial);
+    initial = compound_earned(initial, working.contribution, working.rate_of_return);
+    startAge++;
+    working_months++;
+  }
+
+  while (retire_months < retired.months) {
+    int year = startAge / 12;
+    int month = startAge % 12;
+    printf("Age %3d month %2d you have $%.2f\n", year, month, initial);
+    initial = compound_earned(initial, retired.contribution, retired.rate_of_return);
+    startAge++;
+    retire_months++;
+  }
+}
+
+int main() {
+  retire_info working;
+  working.months = 489;
+  working.contribution = 1000;
+  working.rate_of_return = 0.045;
+
+  retire_info retire;
+  retire.months = 384;
+  retire.contribution = -4000;
+  retire.rate_of_return = 0.01;
+
+  retirement(327, 21345, working, retire);
+  return 0;
+}
