b82d60aef8dcd1a92ed5ec5ba3e4cf67af3ac168
ldw59 <ldw59@duke.edu>
2025-10-07 15:45:50 -0400

    Did assignment 46 step 2

--- 046_eval1/planet_util.c
+++ 046_eval1/planet_util.c
@@ -7,7 +7,27 @@
 
 point_t get_location_at(const planet_t * p, double time) {
   // Step 2: WRITE THIS
+  point_t location;
+  double angular_velocity = (2 * M_PI) / p->year_len;
+  double raw_angle = p->init_pos + angular_velocity * time;
+  double angle_at_time = fmod(raw_angle, 2 * M_PI);
+  if (angle_at_time < 0) {
+    angle_at_time += 2 * M_PI;
+  }
+
+  location.x = p->orbital_radius * cos(angle_at_time);
+  location.y = p->orbital_radius * sin(angle_at_time);
+
+  return location;
 }
 planet_t * find_planet(const planet_list_t * lst, const char * name) {
   //Step 2: WRITE THIS
+  for (size_t i = 0; i < lst->num_planets; i++) {
+    planet_t * planet = lst->planets[i];
+    if (planet && strcmp(planet->name, name) == 0) {
+      return planet;
+    }
+  }
+  fprintf(stderr, "Planet not found\n");
+  exit(EXIT_FAILURE);
 }
