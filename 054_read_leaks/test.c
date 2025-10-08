cdc7cdd0cf3f54a5b3017b30c1be7dd0309ecd18
ldw59 <ldw59@duke.edu>
2025-10-07 23:48:52 -0400

    Did assignment 54

--- 054_read_leaks/test.c
+++ 054_read_leaks/test.c
@@ -4,8 +4,9 @@
 int f(int n) {
   int * p = malloc(2 * sizeof(*p));
   p[0] = n;
-  p[1] = n+2;
+  p[1] = n + 2;
   int ans = p[0] * p[1];
+  free(p);
   return ans;
 }
 
@@ -18,5 +19,7 @@ int main(void) {
   q = malloc(2 * sizeof(*q));
   p = q;
   q = NULL;
+  free(q);
+  free(p);
   return EXIT_SUCCESS;
 }
