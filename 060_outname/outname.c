c5de1cca8d8035d072ae43df3bb1a0e28ef9e964
ldw59 <ldw59@duke.edu>
2025-10-09 20:51:47 -0400

    Did assignment 60

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -5,4 +5,31 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  if (!inputName) {
+    fprintf(stderr, "Input name is NULL\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  char input[strlen(inputName) + 1];
+  strncpy(input, inputName, strlen(inputName) + 1);
+  char * suffix = ".count";
+  int suffix_len = strlen(suffix);
+  char * output = malloc(suffix_len + strlen(inputName) + 1);
+  char * p = input;
+  int i = 0;
+  while (*p != '\0') {
+    output[i] = *p;
+    i++;
+    p++;
+  }
+  int j = 0;
+  while (*suffix != '\0') {
+    output[j + strlen(inputName)] = *suffix;
+    j++;
+    suffix++;
+  }
+
+  output[suffix_len + strlen(inputName)] = '\0';
+
+  return output;
 }
