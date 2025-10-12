a23e54a2c9c0c4dfe4fbcea334ace10633ddab45
ldw59 <ldw59@duke.edu>
2025-10-12 17:50:41 -0400

    Did assignment 62

--- 062_put_together/main.c
+++ 062_put_together/main.c
@@ -31,6 +31,10 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc < 3) {
+    fprintf(stderr, "Input size must be at least 3\n");
+    return EXIT_FAILURE;
+  }
   kvarray_t * kv = readKVs(argv[1]); 
 
  //count from 2 to argc (call the number you count i)
