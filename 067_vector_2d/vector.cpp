73d7def4ee3313a84c9d149080e518b1eecbbf68
ldw59 <ldw59@duke.edu>
2025-10-15 21:23:31 -0400

    Did assignment 67

--- 067_vector_2d/vector.cpp
+++ 067_vector_2d/vector.cpp
@@ -5,3 +5,21 @@
 
 /* write your class implementation in this file
  */
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D result;
+  result.initVector(x + rhs.x, y + rhs.y);
+  return result;
+}
+
+double Vector2D::getMagnitude() const {
+  return std::sqrt(x * x + y * y);
+}
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+void Vector2D::print() const {
+  printf("<%.2f, %.2f>", x, y);
+}
