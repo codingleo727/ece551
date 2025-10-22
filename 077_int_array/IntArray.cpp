eba57c68f25d6063f92a3dd3f208abe48e26dfee
ldw59 <ldw59@duke.edu>
2025-10-22 19:11:24 -0400

    Did assignment 77

--- 077_int_array/IntArray.cpp
+++ 077_int_array/IntArray.cpp
@@ -2,36 +2,84 @@
 #include <assert.h>
 #include <ostream>
 
-IntArray::IntArray() {
-
-}
-IntArray::IntArray(int n) {
-
+IntArray::IntArray(): data(NULL), numElements(0) {
 }
 
-IntArray::IntArray(const IntArray & rhs)  {
-
+IntArray::IntArray(int n): data(new int[n]), numElements(n) {
 }
+
+IntArray::IntArray(const IntArray & rhs): data(new int[rhs.numElements]), numElements(rhs.numElements)  {
+  data = new int[rhs.numElements];
+  for (int i = 0; i < rhs.numElements; i++) {
+    data[i] = rhs.data[i];
+  }
+  numElements = rhs.numElements;
+}
+
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  if (this != &rhs) {
+    delete[] data;
+
+    data = new int[rhs.numElements];
+    for (int i = 0; i < rhs.numElements; i++) {
+      data[i] = rhs.data[i];
+    }
+    numElements = rhs.numElements;
+  }
+  return *this;
 }
+
 const int & IntArray::operator[](int index) const {
+  assert(index < numElements);
+  return data[index];
 }
+
 int & IntArray::operator[](int index) {
+  assert(index < numElements);
+  return data[index];
 }
 
 int IntArray::size() const {
+  return numElements;
 }
 
 bool IntArray::operator==(const IntArray & rhs) const {
+  if (numElements != rhs.numElements) {
+    return false;
+  }
+  
+  for (int i = 0; i < numElements; i++) {
+    if (data[i] != rhs.data[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 bool IntArray::operator!=(const IntArray & rhs) const {
+  if (numElements == rhs.numElements) {
+    for (int i = 0; i < numElements; i++) {
+      if (data[i] != rhs.data[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
+  s << "{";
+  for (int i = 0; i < rhs.size(); i++) {
+    s << rhs[i];
+    if (i < rhs.size() - 1) {
+      s << ", ";
+    }
+  }
+  s << "}";
+  return s;
 }
