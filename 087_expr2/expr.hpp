5503f7efb7c59a9c5853b64679e19a207d0e837c
ldw59 <ldw59@duke.edu>
2025-11-04 17:39:33 -0500

    Did assignment 87

---
+++ 087_expr2/expr.hpp
@@ -0,0 +1,104 @@
+#ifndef EXPR_HPP
+#define EXPR_HPP
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+
+class Expression {
+  public:
+    virtual std::string toString() const = 0;
+    virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+  private:
+    long num;
+  public:
+    NumExpression(long n): num(n) {
+    }
+    virtual std::string toString() const {
+      std::stringstream ss;
+      ss << num;
+      return ss.str();
+    }
+    virtual ~NumExpression() {}
+};
+
+class PlusExpression : public Expression {
+  private:
+    Expression * e1;
+    Expression * e2;
+  public:
+    PlusExpression(Expression * lhs, Expression * rhs): e1(lhs), e2(rhs) {
+    }
+    std::string toString() const {
+      std::stringstream ss;
+      ss << "(" << e1->toString() << " + " << e2->toString() << ")";
+      return ss.str();
+    }
+    virtual ~PlusExpression() {
+      delete e1;
+      delete e2;
+    }
+};
+
+class MinusExpression : public Expression {
+  private:
+    Expression * e1;
+    Expression * e2;
+  public:
+    MinusExpression(Expression * lhs, Expression * rhs): e1(lhs), e2(rhs) {
+    }
+    std::string toString() const {
+      std::stringstream ss;
+      ss << "(" << e1->toString() << " - " << e2->toString() << ")";
+      return ss.str();
+    }
+    virtual ~MinusExpression() {
+      delete e1;
+      delete e2;
+    }
+};
+
+class TimesExpression : public Expression {
+  private:
+    Expression * e1;
+    Expression * e2;
+  public:
+    TimesExpression(Expression * lhs, Expression * rhs): e1(lhs), e2(rhs) {
+    }
+    std::string toString() const {
+      std::stringstream ss;
+      ss << "(" << e1->toString() << " * " << e2->toString() << ")";
+      return ss.str();
+    }
+    virtual ~TimesExpression() {
+      delete e1;
+      delete e2;
+    }
+};
+
+class DivExpression : public Expression {
+  private:
+    Expression * e1;
+    Expression * e2;
+  public:
+    DivExpression(Expression * lhs, Expression * rhs): e1(lhs), e2(rhs) {
+    }
+    std::string toString() const {
+      std::stringstream ss;
+      ss << "(" << e1->toString() << " / " << e2->toString() << ")";
+      return ss.str();
+    }
+    virtual ~DivExpression() {
+      delete e1;
+      delete e2;
+    }
+};
+
+#endif
