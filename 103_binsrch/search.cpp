8c7c8f4634fa0041c2fc87f249ddccc9e62e867d
ldw59 <ldw59@duke.edu>
2025-11-30 23:32:49 -0500

    Did assignment 103

--- 103_binsrch/search.cpp
+++ 103_binsrch/search.cpp
@@ -1,13 +1,17 @@
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  if (high == low || high == low + 1) {
+    return low;
+  }
+
   int mid = (high + low) / 2;
   int mid_value = f->invoke(mid);
 
-  if (high == low + 1) {
-    return low;
+  if (mid_value == 0) {
+    return mid;
   }
-  else if (mid_value <= 0) {
+  else if (mid_value < 0) {
     return binarySearchForZero(f, mid, high);  
   }
   else {
