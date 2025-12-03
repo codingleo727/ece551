98c5759e8c864759bca223c037e96881a4a3000c
ldw59 <ldw59@duke.edu>
2025-12-02 22:32:22 -0500

    Did assignment 109

--- 109_huff_tree/buildTree.cpp
+++ 109_huff_tree/buildTree.cpp
@@ -3,7 +3,9 @@
 Node * buildTree(uint64_t * counts) {
   priority_queue_t pq;
   for (int i = 0; i < 257; ++i) {
-    pq.push(new Node(i, counts[i]));
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
   }
 
   while (pq.size() > 1) {
