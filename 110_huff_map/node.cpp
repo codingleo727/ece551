d55feed69cb8673b21790fa9ccd5a3ed07f0577e
ldw59 <ldw59@duke.edu>
2025-12-05 08:03:22 -0500

    Did assignment 110

--- 110_huff_map/node.cpp
+++ 110_huff_map/node.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
-}
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  if (sym != NO_SYM) {
+    theMap[sym] = b;
+    return;
+  }
 
+  if (left != NULL) {
+    BitString left_bits = b.plusZero();
+    left->buildMap(left_bits, theMap);
+  }
+
+  if (right != NULL) {
+    BitString right_bits = b.plusZero();
+    right->buildMap(right_bits, theMap);
+  }
+}
